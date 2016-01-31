ec1bb6075b80b4a815fa070854989684e0f1350d
ivorobioff <igor.vorobioff@gmail.com>
2016-01-31 10:40:58 +0200

    initial stuff

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingIndicator: UILabel!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordVoice(sender: UIButton) {
+        recordingIndicator.hidden = false
+    
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        recordingIndicator.hidden = true
+    }
 }
 
